91702e67b182dd521a7b778093baebc040dd71bb
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2025-05-20 08:32:47 +0700

    Update InvalidValueException.java

--- electro-server/src/main/java/com/electro/exception/InvalidValueException.java
+++ electro-server/src/main/java/com/electro/exception/InvalidValueException.java
@@ -6,36 +6,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidValueException extends RuntimeException {
 
-    private String fieldName;
-    private Object value;
-    private String reason;
-
     public InvalidValueException(String fieldName, Object value, String reason) {
         super(String.format("Giá trị không hợp lệ cho trường '%s': '%s'. Lý do: %s", fieldName, value, reason));
-        this.fieldName = fieldName;
-        this.value = value;
-        this.reason = reason;
     }
 
     public InvalidValueException(String fieldName, Object value) {
         super(String.format("Giá trị không hợp lệ cho trường '%s': '%s'", fieldName, value));
-        this.fieldName = fieldName;
-        this.value = value;
-    }
-
-    public InvalidValueException(String message) {
-        super(message);
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public Object getValue() {
-        return value;
-    }
-
-    public String getReason() {
-        return reason;
     }
 }
